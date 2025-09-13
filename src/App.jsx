469dba823ac62da127e155f61b4a0d9c937b115d
Ahmet Yusuf Öztürk <72362670+BounStudent@users.noreply.github.com>
2025-09-13 17:41:45 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { Switch, Route, useLocation } from 'react-router-dom';
+import Login from './components/Login';
+
+import './App.css';
+import './components/Layout.css';
+import ErrorPage from './components/ErrorPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <div className="content-section">
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/error">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </div>
+      <LocationDisplay />
+    </>
+  );
+}
+
+export const LocationDisplay = () => {
+  const location = useLocation()
+
+  return <div data-testid="location-display">{location.pathname}</div>
+}
+
+export default App;
